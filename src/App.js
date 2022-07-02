6be06bf9d547df660a566b8d672615e46a635178
rua1hc <tatut90@gmail.com>
2022-07-02 16:42:49 +0700

    s8v5 creating data POST

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import axios from "axios";
 
 import "./App.css";
 
+const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
 class App extends Component {
     state = {
         posts: [],
     };
 
     async componentDidMount() {
-        const { data: posts } = await axios.get(
-            "https://jsonplaceholder.typicode.com/posts"
-        );
+        const { data: posts } = await axios.get(apiEndpoint);
         this.setState({ posts });
     }
 
-    handleAdd = () => {
-        console.log("Add");
+    handleAdd = async () => {
+        const obj = { title: "a", body: "b" };
+        const { data: post } = await axios.post(apiEndpoint, obj);
+
+        console.log(post);
+        const posts = [post, ...this.state.posts];
+        this.setState({ posts });
     };
 
     handleUpdate = (post) => {
