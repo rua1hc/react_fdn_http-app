ad764d3e661ee70e502778d0c3cb5bf0f82c7598
rua1hc <tatut90@gmail.com>
2022-07-02 22:48:36 +0700

    s8v12 extracting http services

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React, { Component } from "react";
-import axios from "axios";
+
+import http from "./services/httpService";
 
 import "./App.css";
-
-axios.interceptors.response.use(null, (error) => {
-    const expectedError =
-        error.response &&
-        error.response.status >= 400 &&
-        error.response.status < 500;
-
-    if (!expectedError) {
-        console.log("Logging error message:", error);
-        alert("Unexpected error occured.");
-    }
-
-    return Promise.reject(error);
-});
 
 const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 
@@ -25,13 +12,13 @@ class App extends Component {
     };
 
     async componentDidMount() {
-        const { data: posts } = await axios.get(apiEndpoint);
+        const { data: posts } = await http.get(apiEndpoint);
         this.setState({ posts });
     }
 
     handleAdd = async () => {
         const obj = { title: "a", body: "b" };
-        const { data: post } = await axios.post(apiEndpoint, obj);
+        const { data: post } = await http.post(apiEndpoint, obj);
 
         console.log(post);
         const posts = [post, ...this.state.posts];
@@ -40,10 +27,10 @@ class App extends Component {
 
     handleUpdate = async (post) => {
         post.title = "Updated put()!";
-        const { data } = await axios.put(apiEndpoint + "/" + post.id, post);
+        const { data } = await http.put(apiEndpoint + "/" + post.id, post);
         console.log(data);
 
-        // await axios.patch(apiEndpoint + "/" + post.id, { title: post.title });
+        // await http.patch(apiEndpoint + "/" + post.id, { title: post.title });
 
         let posts = [...this.state.posts];
         const index = posts.indexOf(post);
@@ -58,7 +45,7 @@ class App extends Component {
         this.setState({ posts });
 
         try {
-            await axios.delete(apiEndpoint + "/" + post.id);
+            await http.delete(apiEndpoint + "/" + post.id);
         } catch (ex) {
             // EXPECTED ERROR: 400 bad request, 404 not found
             // - Display specific error message
